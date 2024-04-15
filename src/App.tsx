6f2eb607e8897db0c6be4bfe4d73fa81c9bbb3d0
Alexander Esbjörnsson <alex.esbj@gmail.com>
2024-04-15 18:36:37 +0200

    added divider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import Navigation from "./components/Navigation"
 import Start from "./components/Start"
 
 function App() {
+  const Divider = () => {
+    return (
+      <div className="divider divider-primary"></div>
+    )
+  }
 
   return (
     <>
       <Navigation />
       <main className="container mx-auto">
         <Start />
+        <Divider />
       </main>
     </>
   )
